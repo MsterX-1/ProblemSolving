353e35dd0b9d4486411b9deab24e935c95d81542
Yousef Ibrahim <yousefsaad40@gmail.com>
2026-07-16 17:56:57 +0300

    Add solution for Two Sum problem

---
+++ 1.two-sum.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i,num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement],i]
+            seen[num] = i
+# @lc code=end
+
